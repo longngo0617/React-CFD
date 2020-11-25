7108d55dfa6bdb97aafc8d456f3423a013071239
Long Ngo <macbook@longngo0617.local>
2020-11-25 18:55:21 +0700

    Add full profile.html

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 const imgWrap = document.querySelector('.slider');
-var imgSlide = new Flickity(imgWrap, {
-    cellAlign: 'left',
-    freeScroll:true,
-    contain: true,
-    prevNextButtons: false,
-    lazyLoad:1,
-    pageDots: false
-});
+if(imgWrap !=null)
+{
+    var imgSlide = new Flickity(imgWrap, {
+        cellAlign: 'left',
+        freeScroll:true,
+        contain: true,
+        prevNextButtons: false,
+        lazyLoad:1,
+        pageDots: false
+    });
+}
+const  clickTab = () => {
+    const tabs = document.querySelectorAll('.tab__title a');
+    let newsList = document.querySelectorAll('.tab__content > *');
+    tabs.forEach( (e, i)=> {
+        e.addEventListener('click', function(k) {
+            k.preventDefault(); 
+            document.querySelector('.js-tab-active').classList.remove('js-tab-active');
+            e.classList.add('js-tab-active');
+
+            //style tab
+            let content = newsList[i];
+            let c =document.querySelector('.tabct.active');
+            c.classList.remove('active');
+            c.style.display = "none"
+            content.classList.add('active');
+            content.style.display ="block";
+        });
+    });
+};
+clickTab();
+
+
